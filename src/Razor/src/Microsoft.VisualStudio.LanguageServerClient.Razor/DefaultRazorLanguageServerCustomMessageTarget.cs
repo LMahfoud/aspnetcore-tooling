a92436ac512bfae42a16ba3192c6206616b8924f
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2020-03-24 01:07:00 -0700

    Added support for auto closing <text> tags in VS LSP

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/DefaultRazorLanguageServerCustomMessageTarget.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/DefaultRazorLanguageServerCustomMessageTarget.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Composition;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
